5c68d51a990d76dfb15f86bc0b3de137f632b6f9
Yousif Masoud <work@hypergraphsoftware.com>
2024-06-02 10:40:59 +0100

    implements dgfly_reboot and set_access_technology_impl

--- connectivity/drivers/cellular/MultiTech/DragonflyNano/PPP/SARA4_PPP_CellularNetwork.cpp
+++ connectivity/drivers/cellular/MultiTech/DragonflyNano/PPP/SARA4_PPP_CellularNetwork.cpp
@@ -16,7 +16,9 @@
  */
 
 #include "SARA4_PPP_CellularNetwork.h"
+#include "rtos/ThisThread.h"
 
+using namespace std::chrono_literals;
 using namespace mbed;
 
 SARA4_PPP_CellularNetwork::SARA4_PPP_CellularNetwork(ATHandler &atHandler, AT_CellularDevice &device) : AT_CellularNetwork(atHandler, device)
@@ -29,6 +31,61 @@ SARA4_PPP_CellularNetwork::~SARA4_PPP_CellularNetwork()
 
 nsapi_error_t SARA4_PPP_CellularNetwork::set_access_technology_impl(RadioAccessTechnology opRat)
 {
-    _op_act = RAT_CATM1;
-    return NSAPI_ERROR_OK;
+    nsapi_error_t ret = NSAPI_ERROR_OK;
+
+    CellularNetwork::AttachStatus status;
+
+    get_attach(status);
+
+    if (status == Attached) {
+        tr_debug("RAT can only be set in detached state");
+        return NSAPI_ERROR_UNSUPPORTED;
+    }
+
+    _at.lock();
+    switch(opRat) {
+        case RAT_EGPRS:
+            _at.at_cmd_discard("+URAT", "=", "%d%d", 9, 8);
+            break;
+        case RAT_CATM1:
+            _at.at_cmd_discard("+URAT", "=", "%d%d", 7, 8);
+            break;
+        case RAT_NB1:
+            _at.at_cmd_discard("+URAT", "=", "%d%d", 8, 7);
+            break;
+        default:
+            _op_act = RAT_UNKNOWN;
+            ret = NSAPI_ERROR_UNSUPPORTED;
+            break;
+    }
+    _at.unlock();
+    if (ret == NSAPI_ERROR_OK) {
+        ret = dgfly_reboot();
+    }
+
+    return ret;
+}
+
+nsapi_error_t SARA4_PPP_CellularNetwork::dgfly_reboot()
+{
+    _at.lock();
+    _at.at_cmd_discard("+CFUN", "=15");
+
+    nsapi_error_t err = NSAPI_ERROR_OK;
+    Timer t1;
+    t1.start();
+
+    while (!(t1.elapsed_time() >= 30s)) {
+        err = _at.at_cmd_discard("E0", "");
+        if (err == NSAPI_ERROR_OK) {
+            break;
+        } else {
+            _at.clear_error();
+            rtos::ThisThread::sleep_for(1s);
+        }
+    }
+
+    t1.stop();
+    _at.unlock();
+    return err;
 }
